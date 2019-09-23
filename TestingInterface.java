0967452b02ac271ada8cf8161a4aadc806d265a6
TrashCoder7 <55701426+TrashCoder7@users.noreply.github.com>
2019-09-23 19:43:25 +0530

    Add files via upload

---
+++ TestingInterface.java
@@ -0,0 +1,21 @@
+package packinterface1;
+
+ public interface TestingInterface {
+	
+	void  push(int x);
+	
+	int pop();
+	
+	
+	
+
+}
+
+ 
+  interface TestingInterface2{
+	 
+	 //int add();
+	 
+	 void divide();
+	 
+ }
